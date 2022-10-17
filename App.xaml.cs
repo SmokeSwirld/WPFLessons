7d2faeaf90f34ac5a8df5aa8c9527c61f52658c1
SmokeSwirld <96337373+SmokeSwirld@users.noreply.github.com>
2022-10-17 18:07:15 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace WPFLessons
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public const int FIELD_SIZE_X = 8; // horisontal
+        public const int FIELD_SIZE_Y = 6; // vertical
+        public const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFLessons.ADO;
+
+namespace WPFLessons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Containers_Click(object sender, RoutedEventArgs e)
+        {
+            new Containers().ShowDialog();
+        }
+
+        private void ContainersHome_Click(object sender, RoutedEventArgs e)
+        {
+            new ContainersHome().ShowDialog();
+        }
+
+        private void Mine_Click(object sender, RoutedEventArgs e)
+        {
+            new Mine().ShowDialog();
+        }
+
+        private void Canvas_Click(object sender, RoutedEventArgs e)
+        {
+            new Canvasmy().ShowDialog();
+        }
+
+        private void Styles_Click(object sender, RoutedEventArgs e)
+        {
+            new Styles().ShowDialog();
+        }
+
+        private void Triggers_Click(object sender, RoutedEventArgs e)
+        {
+            new Triggers().ShowDialog();
+        }
+
+        private void StyTriHome_Click(object sender, RoutedEventArgs e)
+        {
+            new StylesTriggersHome().ShowDialog();  
+        }
+
+        private void DnD_Click(object sender, RoutedEventArgs e)
+        {
+            new DnD().ShowDialog();
+        }
+
+        private void AdoBasics_Click(object sender, RoutedEventArgs e)
+        {
+            new ADO.AdoBasics().ShowDialog();
+        }
+
+        private void Sales_Click(object sender, RoutedEventArgs e)
+        {
+            new ADO.Sales().ShowDialog();   
+        }
+    }
+}
